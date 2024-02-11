bc3affa632b9d2342a13968a91f77c0354b2596e
Lia Alflen <lia.mellon17@gmail.com>
2024-02-11 20:00:27 -0300

    inicial configurations

---
+++ src/test/java/com/br/adocao/pets/adocaopets/AdocaoPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.adocao.pets.adocaopets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdocaoPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
